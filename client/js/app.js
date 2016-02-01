9f9eceef0229a1eeae5f91d02278c92ef0f0407e
Chris Rinaldi <cgrinaldi@gmail.com>
2016-01-31 16:06:19 -0800

    Put application inside of $(document).ready()

--- client/js/app.js
+++ client/js/app.js
@@ -1,13 +1,15 @@
-// Loading dependencies
-var SlotMachineView = require('./views/SlotMachineView.js');
-var WinnerView = require('./views/WinnerView.js');
-var StatsView = require('./views/StatsView.js');
-var InstructionView = require('./views/InstructionView.js');
-var EventEmitter = require('events').EventEmitter;
+$(document).ready(function() {
+  // Loading dependencies
+  var SlotMachineView = require('./views/SlotMachineView.js');
+  var WinnerView = require('./views/WinnerView.js');
+  var StatsView = require('./views/StatsView.js');
+  var InstructionView = require('./views/InstructionView.js');
+  var EventEmitter = require('events').EventEmitter;
 
-// Initializing application
-global.emitter = new EventEmitter();
-SlotMachineView.init();
-WinnerView.init();
-StatsView.init();
-InstructionView.init();
+  // Initializing application
+  global.emitter = new EventEmitter();
+  SlotMachineView.init();
+  WinnerView.init();
+  StatsView.init();
+  InstructionView.init();
+});
